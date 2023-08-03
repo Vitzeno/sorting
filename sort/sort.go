d21a0985941dcf295926122c727df173a400f1f1
Mohamed Yusuf <vitzeno@gmail.com>
2023-08-03 22:02:17 +0100

    cleanup

--- sort/sort.go
+++
@@ -1,9 +0,0 @@
-package sort
-
-import "golang.org/x/exp/constraints"
-
-type Sort[T constraints.Integer] interface {
-	Sort(values []T) []T
-}
-
-type SortFn[T constraints.Integer] func(values []T) []T
